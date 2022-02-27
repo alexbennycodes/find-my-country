61e148a47450d7a21f43d15bbc9179d7357cae61
Alex Benny <alexbenny2811@gmail.com>
2022-02-27 15:05:51 +0530

    updates

--- script.js
+++ script.js
@@ -2,11 +2,18 @@
 
 const countriesContainer = document.querySelector(".countries");
 
+//Btn Selectors
+const showAllBtn = document.querySelector(".show-all");
+const showAsiaBtn = document.querySelector(".show-asia");
+const showAfricaBtn = document.querySelector(".show-africa");
+const showEuropeBtn = document.querySelector(".show-europe");
+const showAmericasBtn = document.querySelector(".show-americas");
+const showOceaniaBtn = document.querySelector(".show-oceania");
+
 const addCountry = function (data) {
-  console.log(data);
   const data_imgSrc = Object.values(data.flags)[1];
   const data_countryName = Object.values(data.name)[0];
-  const data_continent = data.continents || "Undefined";
+  const data_region = data.region || "Undefined";
   const data_capital = data.capital || "Undefined";
   const data_languages = data.languages
     ? Object.values(data.languages).slice(0, 2)
@@ -20,7 +27,7 @@ const addCountry = function (data) {
     <img class="country-img" src="${data_imgSrc}" />
     <div class="country-data">
       <h3 class="country-name">${data_countryName}</h3>
-      <h4 class="country-region">${data_continent}</h4>
+      <h4 class="country-region">${data_region}</h4>
       <p class="country-row"><span>📍</span>${data_capital}</p>
       <p class="country-row"><span>🗣️</span>${data_languages}</p>
         <p class="country-row"><span>👫</span>${data_population}M people</p>
@@ -30,23 +37,53 @@ const addCountry = function (data) {
   countriesContainer.insertAdjacentHTML("beforeend", html);
 };
 
-
 const fetchCountry = function (country) {
   fetch(`https://restcountries.com/v3.1/name/${country}`)
     .then((response) => response.json())
     .then(([data]) => addCountry(data));
 };
 
+// const printAllCountries = function () {
+//   countriesContainer.textContent = "";
+//   fetch("https://restcountries.com/v3.1/all")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       const countries = data;
+//       if (countries) {
+//         for (let country of countries) {
+//           if (country) {
+//             addCountry(country);
+//           }
+//         }
+//       }
+//     });
+// };
 
-fetch("https://restcountries.com/v3.1/all")
-  .then((response) => response.json())
-  .then((data) => {
-    const countries = data;
-    if (countries) {
-      for (let country of countries) {
-        if (country) {
-          addCountry(country);
+const printCountries = function (region) {
+  countriesContainer.textContent = "";
+  fetch(`https://restcountries.com/v3.1/${region}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const countries = data;
+      if (countries) {
+        for (let country of countries) {
+          if (country) {
+            addCountry(country);
+          }
         }
       }
-    }
-  });
+    });
+};
+
+printCountries("all");
+
+showAllBtn.addEventListener("click", () => printCountries("all"));
+showAsiaBtn.addEventListener("click", () => printCountries("region/asia"));
+showAfricaBtn.addEventListener("click", () => printCountries("region/africa"));
+showEuropeBtn.addEventListener("click", () => printCountries("region/europe"));
+showOceaniaBtn.addEventListener("click", () =>
+  printCountries("region/oceania")
+);
+showAmericasBtn.addEventListener("click", () =>
+  printCountries("region/americas")
+);
