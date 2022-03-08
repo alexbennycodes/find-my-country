980683e3c0db54890972fd90cb0a17af0e2378ba
Alex Benny <alexbenny2811@gmail.com>
2022-03-08 22:57:11 +0530

    added async await

--- script.js
+++ script.js
@@ -12,17 +12,21 @@ const showOceaniaBtn = document.querySelector(".show-oceania");
 
 const addCountry = function (data) {
   const data_imgSrc = Object.values(data.flags)[1];
-  const data_countryName = Object.values(data.name)[0];
-  const data_region = data.region || "Undefined";
-  const data_capital = data.capital || "Undefined";
+  const data_countryName = data.name.common;
+  const data_region = data.region ? data.region : "Undefined";
+  const data_capital = data.capital ? data.capital[0] : "Undefined";
   const data_languages = data.languages
     ? Object.values(data.languages).slice(0, 2)
     : ["Undefined"];
-  const data_population = (+data.population / 100000).toFixed(1);
+  const data_population =
+    data.population > 100000
+      ? (+data.population / 100000).toFixed(1) + "M"
+      : data.population > 1000
+      ? (+data.population / 1000).toFixed(1) + "K"
+      : data.population;
   const data_currencies = data.currencies
-    ? Object.values(Object.values(data.currencies)[0])[0]
+    ? Object.values(data.currencies)[0].name
     : ["No Currency"];
-
   const html = `<a href="country.html" class="country">
     <img class="country-img" src="${data_imgSrc}" />
     <div class="country-data">
@@ -30,51 +34,32 @@ const addCountry = function (data) {
       <h4 class="country-region">${data_region}</h4>
       <p class="country-row"><span>📍</span>${data_capital}</p>
       <p class="country-row"><span>🗣️</span>${data_languages}</p>
-        <p class="country-row"><span>👫</span>${data_population}M people</p>
+        <p class="country-row"><span>👫</span>${data_population} people</p>
       <p class="country-row"><span>💰</span>${data_currencies}</p>
     </div>
   </a>`;
   countriesContainer.insertAdjacentHTML("beforeend", html);
 };
 
-const fetchCountry = function (country) {
-  fetch(`https://restcountries.com/v3.1/name/${country}`)
-    .then((response) => response.json())
-    .then(([data]) => addCountry(data));
+const fetchCountry = async function (countryName) {
+  const response = await fetch(
+    `https://restcountries.com/v3.1/name/${countryName}`
+  );
+
+  const data = await response.json();
+  //console.log(...data);
+  addCountry(...data);
 };
 
-// const printAllCountries = function () {
-//   countriesContainer.textContent = "";
-//   fetch("https://restcountries.com/v3.1/all")
-//     .then((response) => response.json())
-//     .then((data) => {
-//       const countries = data;
-//       if (countries) {
-//         for (let country of countries) {
-//           if (country) {
-//             addCountry(country);
-//           }
-//         }
-//       }
-//     });
-// };
-
-const printCountries = function (region) {
+const printCountries = async function (region) {
   countriesContainer.textContent = "";
-  fetch(`https://restcountries.com/v3.1/${region}`)
-    .then((response) => response.json())
-    .then((data) => {
-      const countries = data;
-      if (countries) {
-        for (let country of countries) {
-          if (country) {
-            addCountry(country);
-          }
-        }
-      }
-    });
+  const response = await fetch(`https://restcountries.com/v3.1/${region}`);
+  const data = await response.json();
+  for (let country of data) {
+    console.log(country);
+    addCountry(country);
+  }
 };
-
 printCountries("all");
 
 showAllBtn.addEventListener("click", () => printCountries("all"));
